14a446c95033afb23857273593972202b32b2f67
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-05-18 16:18:11 +0200

    Fix unit tests

--- tests/units/PluginIrcNotificationEventIrc.php
+++ tests/units/PluginIrcNotificationEventIrc.php
@@ -8,39 +8,12 @@ class PluginIrcNotificationEventIrc extends \DbTestCase {
 
    public function testGetTargetField() {
       $data = [];
-      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('phone');
-      $this->array($data)->isIdenticalTo(['phone' => null]);
+      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('users_id');
+      $this->array($data)->isIdenticalTo(['users_id' => null]);
 
-      $data = ['phone' => '+33625885120'];
-      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('phone');
-      $this->array($data)->isIdenticalTo(['phone' => '+33625885120']);
-
-      $user = new \User();
-      $phones = [
-         'name'   => 'The user name',
-         'phone2' => '+33201020405',
-         'phone'  => '+33101020405',
-         'mobile' => '+33601020405'
-      ];
-
-      $user->add($phones);
-      $data = ['users_id' => $user->getID()];
-      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('phone');
-      $this->array($data)->isIdenticalTo(['users_id' => $user->getID(), 'phone' => '+33601020405']);
-
-      unset($phones['mobile']);
-      $phones['name'] = 'Another user name';
-      $user->add($phones);
-      $data = ['users_id' => $user->getID()];
-      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('phone');
-      $this->array($data)->isIdenticalTo(['users_id' => $user->getID(), 'phone' => '+33101020405']);
-
-      unset($phones['phone']);
-      $phones['name'] = 'Yet another one';
-      $user->add($phones);
-      $data = ['users_id' => $user->getID()];
-      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('phone');
-      $this->array($data)->isIdenticalTo(['users_id' => $user->getID(), 'phone' => '+33201020405']);
+      $data = ['users_id' => '121'];
+      $this->string(\PluginIrcNotificationEventIrc::getTargetField($data))->isIdenticalTo('users_id');
+      $this->array($data)->isIdenticalTo(['users_id' => '121']);
    }
 
    public function testCanCron() {
